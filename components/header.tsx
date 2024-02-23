de7262cf1d0e8b8fed741db530f98a97b976c2fa
Rytis Aliukas <rytiss.aliukas@gmail.com>
2024-02-23 21:56:10 +0200

    Antrojo puslapio kodas

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,13 @@
 import React from "react"
-import { UserIcon } from "@heroicons/react/24/outline"
+import { AcademicCapIcon } from "@heroicons/react/24/outline"
 
 export function Header() {
   return (
-    <header className="flex justify-center items-center p-4 border-b border-gray-400 mb-5">
+    <header className="flex justify-between items-center p-4 border-b border-width-20 border-black mb-9">
       <div className="flex items-center">
-        <div className="mr-4 text-2x1 text-black-700 font-bold">
-          Studiju Dienynas
+        <AcademicCapIcon className="h-8 w-8 stroke-black-700" />
+        <div className="mr-5 text-2x1 text-black-700 font-bold">
+          Akademinės studijų pažymos
         </div>
       </div>
     </header>
